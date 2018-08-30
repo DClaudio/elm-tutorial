a688d5ee115f05849c7051b6d1667b607404db4c
DClaudio <david_claudio_t@yahoo.com>
2018-08-30 14:39:25 +0100

    finished elm tutorial

--- src/Msgs.elm
+++ src/Msgs.elm
@@ -3,7 +3,10 @@ module Msgs exposing (..)
 import Models exposing (Player)
 import Navigation exposing (Location)
 import RemoteData exposing (WebData)
+import Http exposing (..)
 
 type Msg
     = OnFetchPlayers (WebData (List Player))
     | OnLocationChange Location
+    | ChangeLevel Player Int
+    | OnPlayerSave (Result Http.Error Player)
